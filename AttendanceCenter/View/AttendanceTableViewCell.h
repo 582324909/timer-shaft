b78d7c86b6d946ce576fb5e137b4ded5b895487b
582324909 <582324909@qq.com>
2016-12-08 14:46:13 +0800

    timer shaft
    timer shaft

---
+++ AttendanceCenter/View/AttendanceTableViewCell.h
@@ -0,0 +1,16 @@
+//
+//  AttendanceTableViewCell.h
+//  AttendanceCentre
+//
+//  Created by admin on 16/3/14.
+//  Copyright © 2016年 admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AttendanceTableViewCell : UITableViewCell
+
+@property (weak, nonatomic) IBOutlet UILabel *timeLabel;
+@property (weak, nonatomic) IBOutlet UILabel *dateLabel;
+
+@end
